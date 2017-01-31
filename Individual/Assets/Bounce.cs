51a160773cf78514c7418e1b06aefaa52c7c7ce9
Ingibergur Sindri Stefnisson <ingibergur13@ru.is>
2017-01-31 15:20:02 +0000

    First commit

---
+++ Individual/Assets/Bounce.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bounce : MonoBehaviour {
+
+
+    
+    float speedx;
+    float speedy;
+    float posX = 0;
+    float posY = 0;
+    bool goUp = false;
+    bool goLeft = true;   
+	// Use this for initialization
+	void Start () {
+        int randLeft = Random.Range(0, 2);
+        goLeft = randLeft == 1 ? true : false;
+        
+        float randX = Random.Range(0f, 2f);
+        float randY = Random.Range(1f, 5f);
+        speedx = randX;
+        speedy = randY;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        posY = transform.position.y;
+        posX = transform.position.x; 
+        if(posY <= -5)
+        {
+            goUp = true;
+        }
+        else if (posY >= 5)
+        {
+            goUp = false;
+        }
+        if(posX <= -10)
+        {
+            goLeft = false;
+        }
+        else if(posX >= 5)
+        {
+            goLeft = true;
+        }
+        if (goUp)
+        {
+            posY += (speedy * Time.deltaTime);
+
+        } else
+        {
+            posY -= (speedy * Time.deltaTime);
+            
+        }
+        if (goLeft)
+        {
+            posX -= (speedx * Time.deltaTime);
+        }
+        else
+        {
+            posX += (speedx * Time.deltaTime);
+        }
+        transform.position = new Vector2(posX, posY);
+    }
+}
---
+++ Individual/Assets/SpawnBall.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnBall : MonoBehaviour {
+
+    Ray ray;
+    RaycastHit hit;
+    public GameObject prefab;
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            GameObject obj = Instantiate(prefab, new Vector3(ray.origin.x, ray.origin.y, ray.origin.z), Quaternion.identity) as GameObject;
+            obj.GetComponent<SpriteRenderer>().color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+
+    }
+}
